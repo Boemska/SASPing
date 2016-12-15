9e12bd44750e5050233bf8b03ff2bbd787604812
bojan88 <bojan@boemskats.com>
2016-12-15 13:15:32 +0100

    Added more tests

--- tests/unit.py
+++ tests/unit.py
@@ -1,13 +1,31 @@
 import unittest
 import re
 
-from sas.functions import getHostUrl
-from sas.settings import getSettings
+import sas.functions
+import sas.settings
 
-class Tests(unittest.TestCase):
+class TestSettings(unittest.TestCase):
+    def testSetException(self):
+        with self.assertRaises(KeyError):
+            sas.settings.set('wrongKey', None)
+
+    def testSetAndGet(self):
+        sas.settings.set('username', 'testUsername')
+        self.assertEqual(sas.settings.get('username'), 'testUsername')
+
+    def testGetAll(self):
+        data = sas.settings.getAll()
+        self.assertTrue(type(data) is dict)
+        self.assertTrue(type(data['execUrl']) is unicode)
+        self.assertTrue(type(data['program']) is unicode)
+        self.assertTrue(type(data['username']) is unicode)
+        self.assertTrue(type(data['password']) is unicode)
+
+
+class TestFunctions(unittest.TestCase):
     def testGetHostUrl(self):
-        hostUrl = re.search('(^http:\/\/.+?)\/', getSettings()['execUrl']).group(1)
-        self.assertEqual(getHostUrl(), hostUrl)
+        hostUrl = re.search('(^https?:\/\/.+?)\/', sas.settings.get('execUrl')).group(1)
+        self.assertEqual(sas.functions.getHostUrl(), hostUrl)
 
 if __name__ == '__main__':
     unittest.main()
