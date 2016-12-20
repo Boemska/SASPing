2d48299f4dd119d4ee945927db85d302cf62cdf7
bojan88 <bojan@boemskats.com>
2016-12-20 18:09:58 +0100

    More setup options

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ setup(
     name='sasping',
     version='0.1',
     author='bojan88',
-    url='https://builds.boemskats.com/bojan/sasping'
+    url='https://builds.boemskats.com/bojan/sasping',
+    packages=['sas']
 )
