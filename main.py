38bd2dd7c75b66e48403848c8a5f2bc113220d55
bojan88 <bojan@boemskats.com>
2016-12-27 10:34:36 +0100

    Adding data (appending on existing), and saving output html page to argv[2]

--- main.py
+++ main.py
@@ -3,15 +3,15 @@
 import sys
 import json
 import csv
+import os
+import re
 
 import sas
 
 def main():
     #python main.py [settings.json path]
     if len(sys.argv) < 3:
-        print('') #for new row
-        sys.stderr.write('Missing arguments. Please read documentation and try again.')
-        print('') #for new row
+        sys.stderr.write('\nMissing arguments. Please read documentation and try again.\n\n')
         sys.exit(1)
 
     try:
@@ -32,11 +32,39 @@ def main():
 
     try:
         keys = ['id', 'status', 'failed test group', 'failed pattern', 'message', 'timestamp', 'execution time', 'had to login']
-        with open(sys.argv[2], "a") as outFile:
-            writer = csv.DictWriter(outFile, keys)
-            if outFile.tell() == 0:
-                writer.writeheader()
-            writer.writerows(testsData)
+        if sys.argv[2].endswith('.csv'):
+            with open(sys.argv[2], "a") as outFile:
+                writer = csv.DictWriter(outFile, keys)
+                if outFile.tell() == 0:
+                    writer.writeheader()
+                writer.writerows(testsData)
+                print 'Created file {0}\n'.format(sys.argv[2])
+        elif sys.argv[2].endswith('.html'):
+            testsArrays = [[test[key] for key in keys] for test in testsData];
+            if os.path.isfile(sys.argv[2]):
+                with open(sys.argv[2], 'r') as finput:
+                    html = finput.read()
+                    jsonData = re.search('<script>var data = (.+)<\/script>', html).group(1)
+                    existingDataArray = json.loads(jsonData)
+                    testsArrays = existingDataArray + testsArrays
+                    finput.close()
+            with open('./report/build/index.html', 'r') as finput:
+                status = os.system('cd ./report && npm run build')
+                if status == 0:
+                    html = finput.read()
+                    finput.close();
+                else:
+                    sys.stderr.write('\nError building index.html. Please try to run "npm run build" from "./reports" manually.\n\n')
+                    sys.exit(1)
+            with open(sys.argv[2], 'w') as foutput:
+                # extract ordered values from testsData
+                jsonOut = json.dumps(testsArrays)
+                foutput.write(html.replace('<!-- data -->', '<script>var data = {0}</script>'.format(jsonOut)))
+                foutput.close();
+            print 'Created file {0}\n'.format(sys.argv[2])
+        else:
+            sys.stderr.write('\nWrong output format. Please read documentation and try again.\n\n')
+            sys.exit(1)
     except IOError as e:
         sys.stderr.write('\n{0}\n\n'.format(str(e)))
         sys.exit(1)
