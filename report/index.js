2bc6415f3ed2330286c749c622a2e9bba7ea338a
bojan88 <bojan@boemskats.com>
2017-01-09 15:35:42 +0100

    New layout

--- report/index.js
+++ report/index.js
@@ -1,7 +1,6 @@
 //colors from style.scss
-var fontCol = '#91959e';
-var mainCol = '#faad39';
-var backCol = '#2f2f2f';
+var mainCol = '#7ab648';
+var gaugeCol = '#ddd';
 
 var timeFormat = d3.time.format("%d/%m/%Y, %H:%M");
 
@@ -33,9 +32,6 @@ function updateData(el, period) {
         return Number(req[6].replace(' seconds', ''));
       };
 
-      var successfulReqs = curData.filter(function(req) {
-        return req[1] === 'success';
-      }).length;
       var loginReqs = curData.filter(function(req) {
         return req[7] === '1'; //had to login = true
       });
@@ -43,57 +39,9 @@ function updateData(el, period) {
         return req[7] === '0'; //had to login = false
       });
 
-      document.querySelector('#total-reqs').innerHTML = curData.length;
-      setGauge(document.querySelector('#succ-req .gauge'),successfulReqs === 0 ? 0 : Math.round((successfulReqs / curData.length).toFixed(2) * 100));
-      document.querySelector('#avg-req-time').innerHTML = curData.length === 0 ? '-' : Math.round(curData.map(execTimeFn).reduce(avgFn, 0).toFixed(2) * 1000) + 'ms';
-      document.querySelector('#avg-login-req-time').innerHTML = loginReqs.length === 0 ? '-' : Math.round(loginReqs.map(execTimeFn).reduce(avgFn, 0).toFixed(2) * 1000) + 'ms';
-      document.querySelector('#avg-wologin-req-time').innerHTML = wloginReqs.length === 0 ? '-' : Math.round(wloginReqs.map(execTimeFn).reduce(avgFn, 0).toFixed(2) * 1000) + 'ms';
-      document.querySelector('#validation-errors').innerHTML = curData.filter(function(req) {
-        return req[1] === 'fail' && req[2];
-      }).length || '-';
-      document.querySelector('#req-errors').innerHTML = curData.filter(function(req) {
-        return req[1] === 'fail' && !req[2];
-      }).length || '-';
-
-      var errorsTableGridRow = document.querySelector('#errors-table-row');
-      var errorsTable = document.querySelector('#errors-table-row table');
-      var errorsTableBody = document.querySelector('#errors-table-row table tbody');
-      errorsTableBody.innerHTML = ''; //remove all rows
-
-      var errorReqs = curData.filter(function(req) {
-        return req[1] === 'fail';
-      });
-      if(errorReqs.length > 0) {
-        errorsTableGridRow.style.display = '';
-        errorReqs.forEach(function(req) {
-          var id = req[0];
-          var execTime = new Date(req[5] * 1000);
-          var row = document.createElement('tr');
-          var col = document.createElement('td');
-          col.innerHTML = id;
-          row.appendChild(col);
-
-          col = document.createElement('td');
-          col.innerHTML = timeFormat(execTime);
-          row.appendChild(col);
-
-          col = document.createElement('td');
-          col.classList.add('has-tooltip');
-          col.setAttribute('title', !req[2] ? req[4] : req[2] + ' -> ' + req[3]);
-          col.innerHTML = !req[2] ?
-                            'Stored Process Application failed to respond' :
-                            'SAS Stored Process application failed to validate';
-          row.appendChild(col);
-
-          errorsTableBody.appendChild(row);
-        });
-      } else {
-        errorsTableGridRow.style.display = 'none';
-      }
-
-      tlite(function(el) {
-        return el.classList.contains('has-tooltip') && {grav: 'se'};
-      });
+      document.querySelector('#avg-response-time').innerHTML = curData.length === 0 ? '-' : Math.round(curData.map(execTimeFn).reduce(avgFn, 0).toFixed(2) * 1000) + 'ms';
+      document.querySelector('#avg-login-time').innerHTML = loginReqs.length === 0 ? '-' : Math.round(loginReqs.map(execTimeFn).reduce(avgFn, 0).toFixed(2) * 1000) + 'ms';
+      // document.querySelector('#avg-ping-time').innerHTML = wloginReqs.length === 0 ? '-' : Math.round(wloginReqs.map(execTimeFn).reduce(avgFn, 0).toFixed(2) * 1000) + 'ms';
 
       drawChart(curData, {login: loginReqs, wlogin: wloginReqs});
     }
@@ -196,7 +144,7 @@ function setGauge(el, percent) {
       el.querySelector('.prec').innerHTML = i + '%';
 
       var deg = i * 360 / 100;
-      el.style['background-image'] = 'linear-gradient('+ (deg <= 180 ? deg+90 : deg-90) +'deg, transparent 50%, '+(deg <= 180 ? fontCol : mainCol)+' 50%),linear-gradient(90deg, '+fontCol+' 50%, transparent 50%)';
+      el.style['background-image'] = 'linear-gradient('+ (deg <= 180 ? deg+90 : deg-90) +'deg, transparent 50%, '+(deg <= 180 ? gaugeCol : mainCol)+' 50%),linear-gradient(90deg, '+gaugeCol+' 50%, transparent 50%)';
 
       if(i++ >= percent) {
         clearInterval(interval);
