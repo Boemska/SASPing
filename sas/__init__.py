4755bfd03687b6693fcc463fe5f037ac159b4c32
bojan88 <bojan@boemskats.com>
2017-01-13 19:13:41 +0100

    Better error handling

--- sas/__init__.py
+++ sas/__init__.py
@@ -17,26 +17,42 @@ def run(config):
     global _settings
     outData = []
     _settings = Settings(config)
-    startTime = time.time()
-    loginStatus = login()
-    loginTime = round(time.time() - startTime, 3)
-    if not(loginStatus):
-        raise RuntimeError('Failed to login.')
-    loginResponse = Response('sasping login request', 'success', timestamp=time.time(), execTime=(str(loginTime) + ' seconds'))
+    try:
+        startTime = time.time()
+        loginStatus = login()
+        loginTime = round(time.time() - startTime, 3)
+        loginResponse = Response('sasping login request', 'success', timestamp=time.time(), execTime=(str(loginTime) + ' seconds'))
+        if not(loginStatus):
+            raise RuntimeError('Failed to login.')
+    except requests.exceptions.Timeout as err:
+        loginResponse = Response('sasping login request', 'fail', message='Request timeout')
+    except Exception as err:
+        loginResponse = Response('sasping login request', 'fail', message=str(err))
+    finally:
+        loginResponse.setTime(startTime, round(time.time() - startTime, 3))
+
     outData.append(dict(loginResponse))
     applications = _settings.get('applications')
     for app in applications:
         for test in [Test(testConfig) for testConfig in app['tests']]:
-            startTime = time.time()
-            response = call(test)
-            response.setTime(startTime, str(round(time.time() - startTime, 3)) + ' seconds')
-            response.setAppName(app['name'])
-            outData.append(dict(response))
+            try:
+                startTime = time.time()
+                response = call(test)
+            except requests.exceptions.Timeout:
+                response = Response(test.get('id'), 'fail', message='Request timeout')
+            except requests.exceptions.ConnectionError:
+                response = Response(test.get('id'), 'fail', message='Name or service not known')
+            except requests.exceptions.RequestException as e:
+                response = Response(test.get('id'), 'fail', message=str(e))
+            finally:
+                response.setTime(startTime, str(round(time.time() - startTime, 3)) + ' seconds')
+                response.setAppName(app['name'])
+                outData.append(dict(response))
 
     return outData
 
 def login():
-    req = _session.get(_settings.getLoginUrl())
+    req = _session.get(_settings.getLoginUrl(), timeout=30)
     hiddenParams = functions.getHiddenParams(req.text)
     return _login(hiddenParams)
 
@@ -51,21 +67,11 @@ def _login(hiddenParams):
     }
     params.update(hiddenParams)
 
-    try:
-        req = _session.post(_settings.getLoginUrl(), params, timeout=30)
-        return req.status_code == 200 and not(functions.needToLogin(req.text))
-    except:
-        return False
+    req = _session.post(_settings.getLoginUrl(), params, timeout=30)
+    return req.status_code == 200 and not(functions.needToLogin(req.text))
 
 def call(test, afterLogin=False):
-    try:
-        req = _session.post(_settings.get('hostUrl') + test.get('execPath'), test.get('execParams'), timeout=30)
-    except requests.exceptions.Timeout:
-        return Response(test.get('id'), 'fail', message='Request timeout')
-    except requests.exceptions.ConnectionError:
-        return Response(test.get('id'), 'fail', message='Name or service not known')
-    except requests.exceptions.RequestException as e:
-        return Response(test.get('id'), 'fail', message=str(e))
+    req = _session.post(_settings.get('hostUrl') + test.get('execPath'), test.get('execParams'), timeout=30)
 
     if req.status_code != 200:
         return Response(test.get('id'), 'fail', message='Request failed - status ' + str(req.status_code))
