16755af156c849dd620b092afb3069573908bdbc
bojan <bojan88@gmail.com>
2017-02-17 12:58:14 +0100

    Using keyword arguments in main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import sys
+import getopt
 import json
 import csv
 import os
@@ -11,13 +12,25 @@ import sas
 from sas.response import Response
 
 def main():
-    #python main.py [settings.json path]
-    if len(sys.argv) < 3:
-        sys.stderr.write('\nMissing arguments. Please read documentation and try again.\n\n')
-        sys.exit(1)
-
     try:
-        config = json.load(open(sys.argv[1]))
+        opts, args = getopt.getopt(sys.argv[1:], 's:o:h', ['settings', 'output', 'help'])
+    except getopt.GetoptError:
+        print 'python main.py -s [settings file path] -o [output dir]'
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt in ('-h', '--help'):
+            sys.stdout.write('\nCollect data about SAS services.\n')
+            sys.stdout.write('\nMandatory arguments:\n')
+            sys.stdout.write('\t-s, --settings        Settings JSON file path.\n')
+            sys.stdout.write('\t-o, --output          Output directory (sasping_data.csv file will be saved there)\n\n')
+            sys.exit(0)
+        elif opt in ('-s', '--settings'):
+            settingsPath = arg
+        elif opt in ('-o', '--output'):
+            outputPath = arg
+    try:
+        config = json.load(open(settingsPath))
     except ValueError as e:
         sys.stderr.write('\nThere is an error in settings.json.\n')
         sys.stderr.write('\nInvalid json with message: {0}\n\n'.format(str(e)))
@@ -34,10 +47,10 @@ def main():
 
     try:
         keys = Response.getKeys()
-        if not(os.path.isdir(sys.argv[2])):
+        if not(os.path.isdir(outputPath)):
             sys.stderr.write('\nWrong output path. Please check if the dir exists.\n\n')
             sys.exit(1)
-        csvFilePath = os.path.join(sys.argv[2], 'sasping_data.csv')
+        csvFilePath = os.path.join(outputPath, 'sasping_data.csv')
         csvFileExists = os.path.isfile(csvFilePath)
         with open(csvFilePath, "a") as outFile:
             writer = csv.DictWriter(outFile, keys)
