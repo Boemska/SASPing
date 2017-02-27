7ab53d3b312f5d38ab49b1876dcfe74db6ea2173
bojan <bojan88@gmail.com>
2017-02-27 12:56:59 +0100

    Moved code for aggregator and test generator to main function

--- aggregate.py
+++ aggregate.py
@@ -8,14 +8,81 @@ import csv
 from sas.response import Response
 from sas.aggregator import Aggregator
 
-try:
-    opts, args = getopt.getopt(sys.argv[1:], 'i:m:h', ['input=', 'maximum=', 'help'])
-except getopt.GetoptError:
-    sys.stdout.write('\npython aggregate.py -i [input file path] -m [maximum number of datapoints]\n')
-    sys.stdout.write('\nRun `python aggregate.py --help` or check the Readme file\n\n')
-    sys.exit(2)
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'i:m:h', ['input=', 'maximum=', 'help'])
+    except getopt.GetoptError:
+        sys.stdout.write('\npython aggregate.py -i [input file path] -m [maximum number of datapoints]\n')
+        sys.stdout.write('\nRun `python aggregate.py --help` or check the Readme file\n\n')
+        sys.exit(2)
 
-maxDatapoints = 100
+    maxDatapoints = 100
+
+    if len(opts) == 0:
+        printHelp()
+
+    for opt, arg in opts:
+        if opt in ('-h', '--help'):
+            printHelp()
+        elif opt in ('-i', '--input'):
+            inFilePath = arg
+        elif opt in ('-m', '--maximum'):
+            maxDatapoints = int(arg)
+
+    csvStrPos = inFilePath.find('.csv');
+    latestFilePath = inFilePath[:csvStrPos] + '_latest.csv'
+    weekFilePath = inFilePath[:csvStrPos] + '_week.csv'
+    monthFilePath = inFilePath[:csvStrPos] + '_month.csv'
+    allTimeFilePath = inFilePath[:csvStrPos] + '_allTime.csv'
+
+    keys = Response.getKeys()
+    keys.remove('failed test group')
+    keys.remove('failed pattern')
+    keys.remove('message')
+
+    descDataFilePath = os.path.split(os.path.abspath(inFilePath))[0] + '/.sasping_agg_data.json'
+
+    def writeAll(data):
+        with open(allTimeFilePath, "w") as file:
+            writer = csv.DictWriter(file, keys)
+            writer.writerows(data)
+            print('\nCreated file {0} with {1} rows\n'.format(allTimeFilePath, len(data)))
+    def writeMonth(data):
+        with open(monthFilePath, "w") as file:
+            writer = csv.DictWriter(file, keys)
+            writer.writerows(data)
+            print('\nCreated file {0} with {1} rows\n'.format(monthFilePath, len(data)))
+    def writeWeek(data):
+        with open(weekFilePath, "w") as file:
+            writer = csv.DictWriter(file, keys)
+            writer.writerows(data)
+            print('\nCreated file {0} with {1} rows\n'.format(weekFilePath, len(data)))
+    def writeLatest(data):
+        with open(latestFilePath, "w") as file:
+            writer = csv.DictWriter(file, keys)
+            writer.writerows(data)
+            print('\nCreated file {0} with {1} rows\n'.format(latestFilePath, len(data)))
+
+
+    with open(inFilePath, "r") as file:
+        # check if file with only latest data exists, and is older than master csv file
+        aggregation = Aggregator(file, maxDatapoints, descDataFilePath)
+
+        if aggregation.getLatestUpdateTime() < int(os.path.getmtime(inFilePath)):
+            if aggregation.shouldUpdateAll():
+                writeAll([row for execution in aggregation.getShrinkedAllTimeData() for row in execution])
+                writeMonth([row for execution in aggregation.getShrinkedMonthData() for row in execution])
+                writeWeek([row for execution in aggregation.getShrinkedWeekData() for row in execution])
+                writeLatest([row for execution in aggregation.getDayData() for row in execution])
+            elif aggregation.shouldUpdateMonth():
+                writeMonth([row for execution in aggregation.getShrinkedMonthData() for row in execution])
+                writeWeek([row for execution in aggregation.getShrinkedWeekData() for row in execution])
+                writeLatest([row for execution in aggregation.getDayData() for row in execution])
+            elif aggregation.shouldUpdateWeek():
+                writeWeek([row for execution in aggregation.getShrinkedWeekData() for row in execution])
+                writeLatest([row for execution in aggregation.getDayData() for row in execution])
+            else:
+                writeLatest([row for execution in aggregation.getDayData() for row in execution])
 
 def printHelp():
     sys.stdout.write('\nThe script will read master CSV file and create 4 aggregated file for each data period.\n')
@@ -25,68 +92,5 @@ def printHelp():
     sys.stdout.write('\t-m, --maximum      Maximum number of datapoints per period displayed in chart.\n')
     sys.exit(0)
 
-if len(opts) == 0:
-    printHelp()
-
-for opt, arg in opts:
-    if opt in ('-h', '--help'):
-        printHelp()
-    elif opt in ('-i', '--input'):
-        inFilePath = arg
-    elif opt in ('-m', '--maximum'):
-        maxDatapoints = int(arg)
-
-csvStrPos = inFilePath.find('.csv');
-latestFilePath = inFilePath[:csvStrPos] + '_latest.csv'
-weekFilePath = inFilePath[:csvStrPos] + '_week.csv'
-monthFilePath = inFilePath[:csvStrPos] + '_month.csv'
-allTimeFilePath = inFilePath[:csvStrPos] + '_allTime.csv'
-
-keys = Response.getKeys()
-keys.remove('failed test group')
-keys.remove('failed pattern')
-keys.remove('message')
-
-descDataFilePath = os.path.split(os.path.abspath(inFilePath))[0] + '/.sasping_agg_data.json'
-
-def writeAll(data):
-    with open(allTimeFilePath, "w") as file:
-        writer = csv.DictWriter(file, keys)
-        writer.writerows(data)
-        print('\nCreated file {0} with {1} rows\n'.format(allTimeFilePath, len(data)))
-def writeMonth(data):
-    with open(monthFilePath, "w") as file:
-        writer = csv.DictWriter(file, keys)
-        writer.writerows(data)
-        print('\nCreated file {0} with {1} rows\n'.format(monthFilePath, len(data)))
-def writeWeek(data):
-    with open(weekFilePath, "w") as file:
-        writer = csv.DictWriter(file, keys)
-        writer.writerows(data)
-        print('\nCreated file {0} with {1} rows\n'.format(weekFilePath, len(data)))
-def writeLatest(data):
-    with open(latestFilePath, "w") as file:
-        writer = csv.DictWriter(file, keys)
-        writer.writerows(data)
-        print('\nCreated file {0} with {1} rows\n'.format(latestFilePath, len(data)))
-
-
-with open(inFilePath, "r") as file:
-    # check if file with only latest data exists, and is older than master csv file
-    aggregation = Aggregator(file, maxDatapoints, descDataFilePath)
-
-    if aggregation.getLatestUpdateTime() < int(os.path.getmtime(inFilePath)):
-        if aggregation.shouldUpdateAll():
-            writeAll([row for execution in aggregation.getShrinkedAllTimeData() for row in execution])
-            writeMonth([row for execution in aggregation.getShrinkedMonthData() for row in execution])
-            writeWeek([row for execution in aggregation.getShrinkedWeekData() for row in execution])
-            writeLatest([row for execution in aggregation.getDayData() for row in execution])
-        elif aggregation.shouldUpdateMonth():
-            writeMonth([row for execution in aggregation.getShrinkedMonthData() for row in execution])
-            writeWeek([row for execution in aggregation.getShrinkedWeekData() for row in execution])
-            writeLatest([row for execution in aggregation.getDayData() for row in execution])
-        elif aggregation.shouldUpdateWeek():
-            writeWeek([row for execution in aggregation.getShrinkedWeekData() for row in execution])
-            writeLatest([row for execution in aggregation.getDayData() for row in execution])
-        else:
-            writeLatest([row for execution in aggregation.getDayData() for row in execution])
+if __name__ == '__main__':
+    sys.exit(main())
--- test_generator.py
+++ test_generator.py
@@ -8,87 +8,91 @@ import os
 from random import getrandbits, randint
 from sas.response import Response
 
-try:
-    opts, args = getopt.getopt(sys.argv[1:], 's:i:m:t:a:o:h', ['start=', 'interval=', 'maximum=', 'tests=', 'apps=', 'output=', 'help'])
-except getopt.GetoptError:
-    sys.stdout.write('\npython test_generator.py -s [start timestamp] -i [interval] -m [maximum] -t [number of tests] -a [number of apps] -o [output dir]\n')
-    sys.stdout.write('\nRun `python test_generator.py --help` or check the Readme file\n\n')
-    sys.exit(2)
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 's:i:m:t:a:o:h', ['start=', 'interval=', 'maximum=', 'tests=', 'apps=', 'output=', 'help'])
+    except getopt.GetoptError:
+        sys.stdout.write('\npython test_generator.py -s [start timestamp] -i [interval] -m [maximum] -t [number of tests] -a [number of apps] -o [output dir]\n')
+        sys.stdout.write('\nRun `python test_generator.py --help` or check the Readme file\n\n')
+        sys.exit(2)
 
-def printHelp():
-    sys.stdout.write('\nThe script will generate tests every X seconds based on the interval argument starting from S timestamp.\n')
-    sys.stdout.write('\nMandatory arguments:\n')
-    sys.stdout.write('\t-s, --start        Start timestamp. Generated data will start from this timestamp.\n')
-    sys.stdout.write('\t-i, --interval     Interval in seconds between consecutive tests.\n')
-    sys.stdout.write('\t-m, --maximum      Maximum number of tests. The script will stop if the number of tests exceeds this argument.\n')
-    sys.stdout.write('\t-t, --tests        Number of tests in one round (an execution of the collector script).\n')
-    sys.stdout.write('\t-a, --apps         Number of dummy applications.\n')
-    sys.stdout.write('\t-o, --output       Output directory (sasping_data.csv file will be saved there).\n')
-    sys.exit(0)
+    def printHelp():
+        sys.stdout.write('\nThe script will generate tests every X seconds based on the interval argument starting from S timestamp.\n')
+        sys.stdout.write('\nMandatory arguments:\n')
+        sys.stdout.write('\t-s, --start        Start timestamp. Generated data will start from this timestamp.\n')
+        sys.stdout.write('\t-i, --interval     Interval in seconds between consecutive tests.\n')
+        sys.stdout.write('\t-m, --maximum      Maximum number of tests. The script will stop if the number of tests exceeds this argument.\n')
+        sys.stdout.write('\t-t, --tests        Number of tests in one round (an execution of the collector script).\n')
+        sys.stdout.write('\t-a, --apps         Number of dummy applications.\n')
+        sys.stdout.write('\t-o, --output       Output directory (sasping_data.csv file will be saved there).\n')
+        sys.exit(0)
 
-if len(opts) == 0:
-    printHelp()
-
-for opt, arg in opts:
-    if opt in ('-h', '--help'):
+    if len(opts) == 0:
         printHelp()
-    elif opt in ('-s', '--start'):
-        fromTimestamp = int(arg)
-    elif opt in ('-i', '--interval'):
-        interval = int(arg)
-    elif opt in ('-m', '--maximum'):
-        maximum = int(arg)
-    elif opt in ('-t', '--tests'):
-        testsPerRun = int(arg)
-    elif opt in ('-a', '--apps'):
-        appsCount = int(arg)
-    elif opt in ('-o', '--output'):
-        outputPath = arg
 
-if len(opts) < 6:
-    sys.stdout.write('\npython test_generator.py -s [start timestamp] -i [interval] -m [maximum] -t [number of tests] -a [number of apps] -o [output dir]\n')
-    sys.stdout.write('\nRun `python test_generator.py --help` or check the Readme file\n\n')
-    sys.exit(2)
+    for opt, arg in opts:
+        if opt in ('-h', '--help'):
+            printHelp()
+        elif opt in ('-s', '--start'):
+            fromTimestamp = int(arg)
+        elif opt in ('-i', '--interval'):
+            interval = int(arg)
+        elif opt in ('-m', '--maximum'):
+            maximum = int(arg)
+        elif opt in ('-t', '--tests'):
+            testsPerRun = int(arg)
+        elif opt in ('-a', '--apps'):
+            appsCount = int(arg)
+        elif opt in ('-o', '--output'):
+            outputPath = arg
 
-currentTimestamp = int(time.time())
-run = 0
+    if len(opts) < 6:
+        sys.stdout.write('\npython test_generator.py -s [start timestamp] -i [interval] -m [maximum] -t [number of tests] -a [number of apps] -o [output dir]\n')
+        sys.stdout.write('\nRun `python test_generator.py --help` or check the Readme file\n\n')
+        sys.exit(2)
 
-out = []
+    currentTimestamp = int(time.time())
+    run = 0
 
-while(fromTimestamp + (interval * run) < currentTimestamp and (maximum > 0 and run < maximum)):
-    # login request
-    response = Response(
-        'sasping login request',
-        'success',
-        timestamp=fromTimestamp + (interval * run),
-        execTime=float(randint(50, 3000))/1000,
-        programExecTime=fromTimestamp + (interval * run)
-    )
-    out.append(dict(response))
+    out = []
 
-    for i in range(testsPerRun):
-        testStatus = 'success' if randint(0, 9) != 9  else 'fail'
-        failGroup = 'mustContain' if bool(getrandbits(1)) else 'cantContain'
+    while(fromTimestamp + (interval * run) < currentTimestamp and (maximum > 0 and run < maximum)):
+        # login request
         response = Response(
-            'test ' + str(i),
-            testStatus,
-            failGroup if testStatus == 'fail' else None,
-            'str' if testStatus == 'fail' else None,
-            'fail message' if testStatus == 'fail' else None,
-            fromTimestamp + (interval * run),
-            float(randint(50, 3000))/1000,
-            False,
-            'test app ' + str(randint(1, appsCount)),
-            fromTimestamp + (interval * run)
+            'sasping login request',
+            'success',
+            timestamp=fromTimestamp + (interval * run),
+            execTime=float(randint(50, 3000))/1000,
+            programExecTime=fromTimestamp + (interval * run)
         )
         out.append(dict(response))
-    run += 1
 
-keys = Response.getKeys()
-csvFilePath = os.path.join(outputPath, 'sasping_data.csv')
-with open(csvFilePath, "w") as outFile:
-    writer = csv.DictWriter(outFile, keys)
-    #writeheader not supported in old versions, so we are using this hack
-    writer.writerow(dict((k, k) for k in keys))
-    writer.writerows(out)
-    print('\nCreated file {0} with {1} rows\n'.format(csvFilePath, len(out)))
+        for i in range(testsPerRun):
+            testStatus = 'success' if randint(0, 9) != 9  else 'fail'
+            failGroup = 'mustContain' if bool(getrandbits(1)) else 'cantContain'
+            response = Response(
+                'test ' + str(i),
+                testStatus,
+                failGroup if testStatus == 'fail' else None,
+                'str' if testStatus == 'fail' else None,
+                'fail message' if testStatus == 'fail' else None,
+                fromTimestamp + (interval * run),
+                float(randint(50, 3000))/1000,
+                False,
+                'test app ' + str(randint(1, appsCount)),
+                fromTimestamp + (interval * run)
+            )
+            out.append(dict(response))
+        run += 1
+
+    keys = Response.getKeys()
+    csvFilePath = os.path.join(outputPath, 'sasping_data.csv')
+    with open(csvFilePath, "w") as outFile:
+        writer = csv.DictWriter(outFile, keys)
+        #writeheader not supported in old versions, so we are using this hack
+        writer.writerow(dict((k, k) for k in keys))
+        writer.writerows(out)
+        print('\nCreated file {0} with {1} rows\n'.format(csvFilePath, len(out)))
+
+if __name__ == '__main__':
+    sys.exit(main())
